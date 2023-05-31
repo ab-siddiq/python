5fe05ade58f8c43d60238bbb88d0a3c0bcb269e7
abu-bakkar-sebpo <siddiq.aiub@gmail.com>
2023-05-31 12:41:27 +0600

    python dictionary

---
+++ dictornary/dictionary.py
@@ -0,0 +1,6 @@
+person={'name':'abu bakkar','age':29,'address':'dhaka','occupation':'programmer'}
+print(person)
+print(person.keys())
+print(person.values())
+for key,values in person.items():
+    print(key,":",values)
