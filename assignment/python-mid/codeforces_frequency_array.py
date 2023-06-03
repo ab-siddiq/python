e80f0eafa3e7c52cee1b5fef763e21534e55ed4e
Md. Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2023-06-03 15:21:59 +0600

    python freq array

--- assignment/python-mid/codeforces_frequency_array.py
+++ assignment/python-mid/codeforces_frequency_array.py
@@ -1,7 +1,10 @@
-A = int(input())
-M = input()
-list = []
-for i in range(A):
-    list.append(input())
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
 
-print(list)
+freq = [0] * (M + 1)
+
+for num in A:
+    freq[num] += 1
+
+for i in range(1, M + 1):
+    print(freq[i])
